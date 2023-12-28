8b37fa988deb71a482470d43fd4679af92253f12
crisri-mac <crifsri@icloud.com>
2023-12-28 21:01:09 +0800

    add shadcn/ui/Select

--- src/components/Layout/MotionLayout.tsx
+++ src/components/Layout/MotionLayout.tsx
@@ -1,20 +1,61 @@
 import Link from "next/link";
 import Inner from "@/components/Layout/Inner";
-import {AnimatePresence} from "framer-motion";
+import Stairs from "@/components/Layout/Stairs";
+import Curve from "@/components/Layout/Curve";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import {useEffect, useState} from "react";
+import * as localforage from "localforage";
 
-export default function MotionLayout({children}: {children: React.ReactNode}) {
 
+const components = {
+    Stairs: Stairs,
+    Inner: Inner,
+    Curve: Curve
+};
+export default function MotionLayout({children}: { children: React.ReactNode }) {
+    const [motionType, setMotionType] = useState("")
+    const Component = components[motionType] ?? Inner;
+    useEffect(() => {
+        localforage.getItem("motionType_local").then(function (value) {
+            setMotionType(value)
+        })
+    }, [])
     return (
+        <>
 
-            <Inner>
+
+            <Component>
+                <div className={"mb-8"} key={"111"}>
+                    <Select value={motionType} onValueChange={(v) => localforage.setItem('motionType_local', v).then(function () {
+                        setMotionType(v)
+                        return localforage.getItem('key');
+                    })}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue  placeholder="choose a motionType"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="Inner">Inner</SelectItem>
+                            <SelectItem value="Stairs">Stairs</SelectItem>
+                            <SelectItem value="Curve">Curve</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
                 <div className="header flex flex-wrap w-full mb-5 ">
                     <Link className={"p-2"} href={"/Motion/RouterMotion"}>Home</Link>
                     <Link className={"p-2"} href={"/Motion/RouterMotion/about"}>About</Link>
                     <Link className={"p-2"} href={"/Motion/RouterMotion/contact"}>Contact</Link>
                 </div>
                 {children}
-            </Inner>
-
+            </Component>
+        </>
 
     )
 }
---
+++ src/components/Layout/Stairs/index.tsx
@@ -0,0 +1,89 @@
+import { motion } from 'framer-motion'
+
+export default function Inner({ children }: { children: React.ReactNode }) {
+    const anim = (variants: any, custom?: any) => {
+        return {
+            initial: "initial",
+            animate: "enter",
+            exit: "exit",
+            variants,
+            custom
+        }
+    }
+    const opacity = {
+        initial: {
+            opacity: 0
+        },
+        enter: {
+            opacity: 1,
+            transition : {
+                duration: 2
+            }
+        },
+        exit: {
+            opacity: 1
+        }
+    }
+
+    // 动画从 top 0 的位置往下
+    const expand = {
+        initial: {
+            top: 0
+        },
+        enter: (i: number) => ({
+            top: "100%",
+            transition : {
+                duration: 0.3,
+                delay: i * 0.1
+            },
+            transitionEnd: {
+                // 动画结束后设置高度为 0
+                // 为exit 动画做准备 需要 height: 0 top:0
+                height: 0,
+                top:0
+            }
+        }),
+        exit: (i:number) => ({
+            // 结束动画
+            height: "100%",
+            transition : {
+                duration: 0.3,
+                delay: i * 0.1
+            },
+        })
+    }
+    const nbOfStairs = 5;
+
+    // 过渡过程中 的过度 overlay 对效果的影响很显著
+    const overlay= {
+        initial: {
+            opacity: 0.5
+        },
+        enter: {
+            opacity: 0
+        },
+        exit: {
+            opacity: 0.5
+        }
+    }
+    return <div className={"page stairs bg-black h-screen"}>
+
+        <motion.div {...anim(overlay)} className="overlay fixed w-screen h-screen top-0 bg-black pointer-events-none"></motion.div>
+        {/* 这里注意 pointer-events: none 否则点击事件穿透不了 */}
+        <div className={"transition-container fixed w-screen h-screen top-0 flex pointer-events-none"}>
+            {
+                [...Array(nbOfStairs)].map((_, i) => {
+                    // 根据传递的下标 可以调整动画从左导游 还是从右到左
+                    return <motion.div {...anim(expand, nbOfStairs - i)} key={i} className={"relative bg-black h-full grow"}></motion.div>
+                })
+            }
+        </div>
+
+        <motion.div className={"bg-white h-full"}>
+            <motion.div className={"p-10"}>
+                {children}
+            </motion.div>
+        </motion.div>
+    </div>
+
+}
